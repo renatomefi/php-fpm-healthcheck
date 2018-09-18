972097fa2e3cd2636ccb7998833ac131a2f03bb1
Renato Mefi <gh@mefi.in>
2018-09-18 18:25:27 +0200

    Test if the script works on php-fpm socket
    Fixes #5
    The test will change temporarily fpm to run on socket, run the
    healthcheck and revert it.

--- test/testinfra/test_fpm.py
+++ test/testinfra/test_fpm.py
@@ -16,6 +16,22 @@ def test_exit_when_no_status_page_is_configured(host):
     host.run("sed -i /usr/local/etc/php-fpm.d/zz-docker.conf -e '/;pm.status_path/ s/^;*//'")
     host.run("kill -USR2 1")
 
+@pytest.mark.php_fpm
+def test_fpm_on_socket(host):
+    # change fpm to socket
+    host.run("sed -i /usr/local/etc/php-fpm.d/zz-docker.conf -e '/^listen/ s/.*/listen = \\/var\\/run\\/php-fpm.sock/'")
+    host.run("kill -USR2 1")
+    
+    cmd = host.run("FCGI_CONNECT=/var/run/php-fpm.sock php-fpm-healthcheck -v")
+    assert cmd.rc == 0
+    assert "Trying to connect to php-fpm via:" in cmd.stdout
+    assert "status output:" in cmd.stdout
+    assert "pool:" in cmd.stdout
+
+    # change fpm back to port 9000
+    host.run("sed -i /usr/local/etc/php-fpm.d/zz-docker.conf -e '/^listen/ s/.*/listen = 9000/'")
+    host.run("kill -USR2 1")
+
 @pytest.mark.alpine
 def test_exit_when_fpm_is_not_reachable_apk(host):
     cmd = host.run("FCGI_CONNECT=localhost:9001 php-fpm-healthcheck -v")
