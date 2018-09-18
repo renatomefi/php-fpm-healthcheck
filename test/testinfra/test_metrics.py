34c4c3f8ae90710e2e330a507ed5bb1d119f11f5
Renato Mefi <gh@mefi.in>
2018-09-18 20:20:31 +0200

    Test failed metrics
    Fixes #4

--- test/testinfra/test_metrics.py
+++ test/testinfra/test_metrics.py
@@ -1,6 +1,20 @@
 import pytest
 
 @pytest.mark.php_fpm
+def test_metric_fail_accepted_conn(host):
+    cmd = host.run("php-fpm-healthcheck --accepted-conn=0")
+    assert cmd.rc == 1
+    assert "'accepted conn' value '1' is greater than expected '0'" in cmd.stderr
+
+@pytest.mark.php_fpm
+def test_metric_fail_accepted_conn_with_other_metrics(host):
+    cmd = host.run("php-fpm-healthcheck --verbose --listen-queue=10 --max-active-processes=10 --accepted-conn=0")
+    assert cmd.rc == 1
+    assert "'accepted conn' value '2' is greater than expected '0'" in cmd.stderr
+    assert "'listen queue' value" in cmd.stdout
+    assert "'max active processes' value" in cmd.stdout
+
+@pytest.mark.php_fpm
 def test_metric_accepted_conn(host):
     cmd = host.run("php-fpm-healthcheck -v")
     assert cmd.rc == 0
