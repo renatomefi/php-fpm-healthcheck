dc64497d4ab50be98e6de93f0eb7d902bf7c014d
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-01-17 22:20:38 -0300

    Check for timeout return code

--- test/testinfra/test_timeout.py
+++ test/testinfra/test_timeout.py
@@ -25,3 +25,13 @@ def test_timeout_invalid_value(host):
     assert cmd.rc == 3
     assert "--timeout" in cmd.stderr
     assert "option value must be an integer" in cmd.stderr
+
+# TODO test timeout command not found
+
+@pytest.mark.php_fpm
+def test_timeout_sleep(host):
+    host.run("echo '<?php sleep(2);' > /var/www/html/sleep.php")
+    cmd = host.run("FCGI_STATUS_PATH=/var/www/html/sleep.php php-fpm-healthcheck --timeout=1")
+    # cmd.rc == 124 (coreutils)
+    # cmd.rc == 129 (busybox)
+    assert cmd.rc in [124, 129]
